3790fe5546aa0071ffa68c051b366451d8e28625
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-04-23 19:00:56 +0200

    remove console

--- compile.bat
+++ compile.bat
@@ -5,5 +5,5 @@ del resource.o
 windres resource.rc -O coff -o resource.o
 cd ..
 cd ..
-gcc src/main.c src/resource/resource.o -lgdi32 -luser32 -lcomctl32 -o catchzeblock.exe
+gcc src/main.c src/resource/resource.o -lgdi32 -luser32 -lcomctl32 -o catchzeblock.exe -mwindows
 catchzeblock.exe
